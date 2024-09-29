30924cdc9f369f916588e704d43baafbe9752d29
isado <isadorabellaguarda@gmail.com>
2024-09-29 16:52:02 -0300

    adicionando text block

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,12 @@ public class Main {
     public static void regrasDoJogo(){
         System.out.println("\n~~~Bem-vindos ao jogo de batalha naval!~~~".toUpperCase());
         System.out.println("São dois jogadores (ou um jogador e o computador)");
-        System.out.println("Cada jogador terá:\n" +
-                "     * Um navio ocupando 4 espaços\n" +
-                "     * Dois navios ocupando 3 espaços\n" +
-                "     * Três navios ocupando 2 espaços\n" +
-                "     * Quatro navios ocupando 1 espaço");
+        System.out.println("""
+                Cada jogador terá:
+                     * Um navio ocupando 4 espaços
+                     * Dois navios ocupando 3 espaços
+                     * Três navios ocupando 2 espaços
+                     * Quatro navios ocupando 1 espaço""");
         System.out.println("Também é possível escolher se deseja inserir os barcos de forma manual ou de forma automática.");
         System.out.println("Tiros na água imprimem '*', tiros em navios imprimem o número da quantidade de posições do navio.");
         System.out.println("Vence quem afundar todos os navios do adversário primeiro.");
